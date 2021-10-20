85574121ce545089d5c79b94cbb1fb3e58524240
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-20 20:16:35 +0200

    add templates

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home,todo_list, todo_add
 
 urlpatterns = [
     path('', home, name='home'),
+    path('list/', todo_list, name='list'),
+    path('add/', todo_add, name='add'),
+    
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoAddForm
 
 
 def home(request):
     return render(request, "todo/home.html")
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos' : todos
+    }
+    
+    return render(request, "todo/todo_list.html", context)
+
+def todo_add(request):
+    
+    form = TodoAddForm()
+    
+    if request.method == "POST":
+        form = TodoAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    
+    context = {
+        'form' : form,
+    }
+    
+    return render(request, "todo/todo_add.html", context)
+
+def todo_update(request,id):
+    todo = Todo.objects.get(id=id)
+    form = TodoAddForm(instance=todo)
+    
+    if request.method == 'POST':
+        form = TodoAddForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    context = {
+        "todo": todo,
+        "form": form,
+    }
+    
+    return render(request, "todo/todo_update.html", context)
+    
