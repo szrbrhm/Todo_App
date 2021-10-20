42597629971b4465bffd4f3c265c8f89746d8921
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-20 19:21:06 +0200

    create Todo models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=40)
+    complete = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('-created_date',)
+    
+    def __str__(self):
+        return self.title
